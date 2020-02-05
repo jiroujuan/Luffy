8616572cc87115db13a6d4bce7a4ee72809c7e5b
袁粤坤 <yuekun.yuan@downtown8.com>
2020-02-05 20:36:04 +0800

    0.1.5

--- src/framework/app.ts
+++ src/framework/app.ts
@@ -81,7 +81,7 @@ export interface AppOptions {
 const DEFAULT_OPTIONS: AppOptions = {
   host: '0.0.0.0',
   port: 8080,
-  appDir: path.join(path.resolve(__dirname, '../../../../app')),
+  appDir: path.join(path.resolve(__dirname, '../../../../bin')),
   ctrlDir: 'controllers',
   authDir: 'auths',
   apiPrefix: '/api',
